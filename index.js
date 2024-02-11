15f1b0d6e24ee15ba95d5dbabe0e35a0a00f11f3
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-02-11 19:52:51 -0300

    err

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-import app from "./app/config.js";
-import "./routes.js"
-
+const app = require("./app/config.js")
+require("./routes.js")
 app.listen(3000, (err)=>{
   if(err){
     console.log(err)
--- routes.js
+++ routes.js
@@ -1,14 +1,15 @@
-import express from "express"
-import app from "./app/config.js"
-import { engine } from "express-handlebars"
-
-app.engine("hbs", engine({
+const express = require("express")
+const app = require("./app/config.js")
+const exbhs = require("express-handlebars")
+const path = require("path")
+app.engine("hbs", exbhs.engine({
   extname: ".hbs",
   defaultLayout: "main",
+  layoutsDir: path.join(__dirname + "/views/layouts")
 }))
 app.set("view engine", "hbs")
-app.set("views", express.static("views"))
+app.use("views", express.static(path.join(__dirname + "/views")))
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
   res.render("home")
 })
